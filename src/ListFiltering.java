308b920dea07a7d79e4ab60da927ab6190aeef10
Maho Miyata <mahomiyata@users.noreply.github.com>
2023-07-24 21:48:56 +0900

    codewars - refactor

--- src/ListFiltering.java
+++ src/ListFiltering.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ListFiltering {
     public static List<Object> filterList(final List<Object> list) {
         // Return the List with the Strings filtered out
-        return list.stream().filter(item -> item instanceof Integer).toList();
+        return list.stream().filter(Integer.class::isInstance).toList();
     }
 }
 
